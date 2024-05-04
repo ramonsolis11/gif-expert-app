4a36b1bd2f84722aa197541c1450cedb6ea71880
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2024-05-04 17:39:01 -0600

    feat: add category functionality to GifExpertApp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,13 @@
+import { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+    const onAddCategory = () => {
+        setCategories(['HunterXHunter', ...categories]);
+    }
+
     return (
         <>
             {/* titulo */}
@@ -8,6 +16,14 @@ export const GifExpertApp = () => {
             {/* Input*/}
 
             {/* Listado de Gif */}
+            <button onClick={ onAddCategory }>Agregar</button>
+            <ol>
+                {
+                    categories.map( category => {
+                        return <li key={ category }>{ category }</li>
+                    })
+                }
+            </ol>
 
             {/* Gif Item */}
 
