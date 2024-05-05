675f8e398134876208583ecbfafa87afeac497c7
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2024-05-04 18:07:26 -0600

    "add AddCategory component to GifExpertApp"

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
 
@@ -14,6 +15,7 @@ export const GifExpertApp = () => {
             <h1>GifExpertApp</h1>
 
             {/* Input*/}
+            <AddCategory />
 
             {/* Listado de Gif */}
             <button onClick={ onAddCategory }>Agregar</button>
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+
+export const AddCategory = () => {
+
+    const [inputValue, setInputValue] = useState('One Punch');
+
+    const onInputChange = ({ target }) => {
+        setInputValue(target.value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+
+
+
+    return (
+        <form onSubmit={ (event) => onSubmit(event)}>
+        <input 
+        type="text" 
+        placeholder="Buscar Gif..."
+        value={ inputValue }
+        onChange={ onInputChange }
+        />
+        </form>
+    )
+}
