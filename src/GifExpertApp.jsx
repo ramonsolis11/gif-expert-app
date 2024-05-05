33b8afaa7fed408b3c713f002f083a3d68bc9bb8
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2024-05-05 16:32:17 -0600

    feat: remove unnecessary code in GifExpertApp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -29,7 +29,6 @@ export const GifExpertApp = () => {
                         return <li key={ category }>{ category }</li>
                     })
                 }
-                
             </ol>
 
             {/* Gif Item */}
